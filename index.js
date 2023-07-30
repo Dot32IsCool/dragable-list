2a3c9251fb0e0238ef7ee0187938980c33e00232
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-07-30 09:25:24 +0800

    Semi-functional smooth animation

--- index.js
+++ index.js
@@ -53,29 +53,61 @@ function moveElement (target, event) {
 	// update the posiion attributes
 	target.setAttribute('data-y', y)
 
-	reorder(target)
+	// document.startViewTransition(() => {
+		reorder(target)
+	// });
 }
 
   function reorder (target) {
 	// var target = event.target.parentNode
 	// get the parent element
 	var parent = target.parentNode
+
+	// Loop over all elements in the list
+	for (var i = 0; i < parent.children.length; i++) {
+		// Remove the class that animates the element
+		parent.children[i].classList.remove("boom-down")
+		parent.children[i].classList.remove("boom-up")
+	}
+		
 	// get the index of the element
 	var index = Array.prototype.indexOf.call(parent.children, target)
 	// get the height of the element
 	var computedHeight = target.getBoundingClientRect().height
 	// get how many elements it is down in the list
-	var newIndex = Math.max(index + Math.ceil(target.getAttribute('data-y') / computedHeight), 0)
+	let dataY = target.getAttribute('data-y')
+	var newIndex = Math.max(index + Math.ceil(dataY / computedHeight), 0)
 	// set index of the element
 	if (newIndex < parent.children.length) {
 		parent.insertBefore(target, parent.children[newIndex]);
+		// console.log("boom")
+		if (newIndex-1 != index && newIndex != index) {
+			// console.log("boom")
+			if (dataY > 0) {
+				console.log("boom-down")
+				// Find element that is above the target
+				var aboveElement = parent.children[newIndex-2]
+				// Set its position to the bottom of the target
+				// aboveElement.style.transform = 'translate(0px, ' + computedHeight + 'px)'
+				// Give the element a class to animate it
+				aboveElement.classList.add("boom-down")
+			} else {
+				console.log("boom-up")
+				// Find element that is below the target
+				var belowElement = parent.children[newIndex+1]
+				// Set its position to the top of the target
+				// belowElement.style.transform = 'translate(0px, ' + -computedHeight + 'px)'
+				// Give the element a class to animate it
+				belowElement.classList.add("boom-up")
+			}
+		}
 	} else {
 		parent.appendChild(target)
 		target.setAttribute('data-y', Math.min(target.getAttribute('data-y'), 0))
 	}
 	// make the translation relative to the new position, if it is not the first element
 	if (newIndex > 0) {
-		console.log(newIndex)
+		// console.log(newIndex)
 		target.style.transform = 'translate(0px, ' + (target.getAttribute('data-y') % computedHeight) + 'px)'
 		// update the posiion attributes
 		target.setAttribute('data-y', target.getAttribute('data-y') % computedHeight)
@@ -104,6 +136,7 @@ var myFunction = function() {
 
 for (var i = 0; i < list_elements.length; i++) {
 	list_elements[i].addEventListener('click', myFunction, false)
+	// list_elements[i].style.viewTransitionName = "list" + i
 }
 
 // remove selected class from all elements
